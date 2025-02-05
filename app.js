d5185ded8eb34a394c3295026ef1a8264c36b628
Alef Apolinario <apolinarioalef69@gmail.com>
2025-02-05 06:45:22 -0300

    Função para atualizar lista na tela

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-// Array para conter os nomes dos amigos
 let listaAmigos = []
 
-// Função para adicionar amigos a lista
 function adicionarAmigo() {
     let inputAmigo = document.getElementById("amigo")
     let nomeAmigo = inputAmigo.value.trim()
 
-    // Validar que o Input não está vazio
     if (nomeAmigo === "") {
         alert ("Por favor, digite um nome.")
-        return // o Return interrompe a execução da função
+        return
     }
 
-    // Validar se o nome está duplicado
     if (listaAmigos.includes(nomeAmigo)) {
         alert ("Este nome já foi adicionado")
         return
     }
 
-    // Adicionar um nome a lista
     listaAmigos.push(nomeAmigo)
-
-    // Limpar a lista quando o site por iniciado
     inputAmigo.value = ""
-
-    // Atualizar a lista no HTML
     atualizarLista()
+}
+
+function atualizarLista() {
+    let arrayAmigos = document.getElementById("listaAmigos")
+    arrayAmigos.innerHTML = ""
+
+    for (let i = 0; i <listaAmigos.length; i++) {
+        let li = document.createAttribute("li")
+        li.textContent = listaAmigos[i]
+        arrayAmigos.appendChild(li)
+    }
 }
