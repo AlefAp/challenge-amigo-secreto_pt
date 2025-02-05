5e54f45f2c78ccffe50ddbb9dd6c3c5c4be813ef
Alef Apolinario <apolinarioalef69@gmail.com>
2025-02-05 06:35:28 -0300

    Função para adicionar amigos a lista

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Array para conter os nomes dos amigos
+let listaAmigos = []
+
+// Função para adicionar amigos a lista
+function adicionarAmigo() {
+    let inputAmigo = document.getElementById("amigo")
+    let nomeAmigo = inputAmigo.value.trim()
+
+    // Validar que o Input não está vazio
+    if (nomeAmigo === "") {
+        alert ("Por favor, digite um nome.")
+        return // o Return interrompe a execução da função
+    }
+
+    // Validar se o nome está duplicado
+    if (listaAmigos.includes(nomeAmigo)) {
+        alert ("Este nome já foi adicionado")
+        return
+    }
+
+    // Adicionar um nome a lista
+    listaAmigos.push(nomeAmigo)
+
+    // Limpar a lista quando o site por iniciado
+    inputAmigo.value = ""
+
+    // Atualizar a lista no HTML
+    atualizarLista()
+}
